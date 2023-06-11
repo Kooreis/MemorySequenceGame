6b5ed2a763fc9a607a1f25c3aed1f64a60ad498c
Kooreis <kooreis.official@gmail.com>
2023-06-11 11:19:24 -0500

    Initial setup of the MemorySequenceGame program. A list to hold the sequence of numbers, a random number generator, and a score variable are initialized.

--- Memory_Sequence_Game.cs
+++ Memory_Sequence_Game.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,35 +12,6 @@ namespace MemorySequenceGame
             List<int> sequence = new List<int>();
             Random random = new Random();
             int score = 0;
-
-            while (true)
-            {
-                sequence.Add(random.Next(1, 5));
-
-                foreach (var number in sequence)
-                {
-                    Console.WriteLine(number);
-                    Thread.Sleep(1000);
-                    Console.Clear();
-                    Thread.Sleep(500);
-                }
-
-                foreach (var number in sequence)
-                {
-                    Console.Write("Enter the next number in the sequence: ");
-                    int input = Convert.ToInt32(Console.ReadLine());
-
-                    if (input != number)
-                    {
-                        Console.WriteLine($"Game Over! Your score was: {score}");
-                        return;
-                    }
-
-                    score++;
-                    Console.Clear();
-                }
-            }
         }
     }
 }
-```
