9f7a21af136c490b5c5060b9fccdacaba9c44c9f
Kooreis <kooreis.official@gmail.com>
2023-06-12 10:19:24 -0500

    Defines the main class and main method, initializes the list of colors, the game sequence, the scanner for user input, and the round counter.

--- Memory_Sequence_Game.java
+++ Memory_Sequence_Game.java
@@ -1,5 +1,6 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+public class MemorySequenceGame {
+    public static void main(String[] args) {
+        List<String> colors = Arrays.asList("Red", "Blue", "Green", "Yellow", "Purple", "Orange");
+        List<String> gameSequence = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int round = 0;
