98418e14260301f9804fbb9dc027e091d334bd80
Kooreis <kooreis.official@gmail.com>
2023-06-12 10:19:24 -0500

    Added a function to get a list of numbers from the user. The number of inputs is based on the difficulty level.

--- Memory_Sequence_Game.py
+++ Memory_Sequence_Game.py
@@ -1,4 +1,3 @@
-import random
-
-def generate_sequence(difficulty):
-    return [random.randint(1, 101) for _ in range(difficulty)]
+def get_list_from_user(difficulty):
+    print("After seeing the numbers, enter the numbers you saw, each followed by Enter.")
+    return [int(input()) for _ in range(difficulty)]
